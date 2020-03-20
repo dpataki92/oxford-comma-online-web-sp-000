7143b0865ee4024aefb85130a3feceb7586553a4
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-20 22:57:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,23 +1,5 @@
 def oxford_comma(array)
 
-  new_arr = array.collect {|el|
-
-      if array.length == 1 || array.index(el) == array.length-1
-        el = el
-      elsif array.index(el) == array.length - 2
-        array.length != 2 ? el += ", and " : el += " and "
-      else
-        el += ", "
-      end
-  }
-
-  new_arr.join
-
-end
-
-# refactoring code above
-
-def oxford_comma_simple(array)
   if array.length == 1
     array.join
   elsif array.length == 2
@@ -29,3 +11,4 @@ def oxford_comma_simple(array)
   end
 
 end
+
