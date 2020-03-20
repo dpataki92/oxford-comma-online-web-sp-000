be00b7ba0cf87bdfe3ba8ebac5f000dcc63483b9
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-20 22:58:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,11 @@ def oxford_comma(array)
 
   if array.length == 1
     array.join
+
   elsif array.length == 2
     array[0] << " and "
     array.join
+
   elsif array.length > 2
     array[-1] = "and " + array[-1]
     array.join(", ")
